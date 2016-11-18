55fc840ba3def6585a1135c7c6a305edf7b51d17
walston <nathanielwalston@gmail.com>
2016-11-18 09:10:58 -0800

    make the script work even in the wrong directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,15 @@
 echo "Enter a WordPress theme name:"
 read name
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 vers=$(docker-compose -v)
 dc_exists=$?
 
 if [[ $dc_exists == 0 ]]; then
   docker-compose up -d
-  ln -s $PWD/src/wp-content/plugins $PWD/
-  mkdir $PWD/src/wp-content/themes/$name
-  ln -s $PWD/src/wp-content/themes/$name $PWD/
+  ln -s $DIR/src/wp-content/plugins $DIR/
+  mkdir $DIR/src/wp-content/themes/$name
+  ln -s $DIR/src/wp-content/themes/$name $DIR/
 else
   echo "This script relies on docker-compose as a global binary"
 fi
