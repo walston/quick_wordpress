0657c73a9a2fa1d868677e7336d3f8224cef4bbe
walston <nathanielwalston@gmail.com>
2016-11-18 08:51:35 -0800

    Create the theme folder before the symlink, other wise the link doen't work

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ dc_exists=$?
 if [[ $dc_exists == 0 ]]; then
   docker-compose up -d
   ln -s $PWD/src/wp-content/plugins $PWD/
+  mkdir $PWD/src/wp-content/themes/$name
   ln -s $PWD/src/wp-content/themes/$name $PWD/
 else
   echo "This script relies on docker-compose as a global binary"
